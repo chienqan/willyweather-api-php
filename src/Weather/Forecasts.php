cb90a769f2d53edb43b534a382bd016a7f328fa8
Chien Nguyen <deirpes@gmail.com>
2018-02-11 08:21:35 +0700

    Add reduce array for forecasts

--- src/Weather/Forecasts.php
+++ src/Weather/Forecasts.php
@@ -4,6 +4,20 @@ namespace WillyWeatherAPI\Weather;
 
 class Forecasts extends WeatherCollection
 {
+    public function reduceArray($array)
+    {
+        $array = array_values($array);
+
+        $days = $array[0]['days'];
+        $entries = array();
+        foreach ($days as $day) {
+            $entries[] = $day['entries'];
+        }
+    
+        $entries = array_reduce($entries, 'array_merge', array());
+        return $entries;
+    }
+
     public function getWind()
     {
         return $this->willy->getForecasts("wind");
